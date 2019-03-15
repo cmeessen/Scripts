b4b7e1fd864474216e855bcf73edcce14165ee51
Christian Meeßen <14222414+cmeessen@users.noreply.github.com>
2019-03-15 20:52:27 +0100

    Make GMT folder

---
+++ GMT/README.md
@@ -0,0 +1,10 @@
+# GMT
+
+Collection for convenient plotting and conversion from the terminal, based on
+[GMT](https://gmt.soest.hawaii.edu/). Since GMT frequently changes their syntax
+the scripts are sorted by version number.
+
+**NOTE:** This collection in particular is pretty basic and I created it when
+version 5.4 was the latest. The compatibility of the scripts in the versions
+before is not granted. Also, scripts that were written for GMT 5.4 were not
+back-ported.
--- paraview2gmsh/paraview2gmsh.py
+++ paraview2gmsh/paraview2gmsh.py
@@ -1,3 +1,20 @@
+################################################################################
+#                     Copyright (C) 2019 by Christian Meeßen                   #
+#                                                                              #
+#                        This file is part of GMTScripts.                      #
+#                                                                              #
+#       GMTScripts is free software: you can redistribute it and/or modify     #
+#     it under the terms of the GNU General Public License as published by     #
+#           the Free Software Foundation version 3 of the License.             #
+#                                                                              #
+#      GMTScripts is distributed in the hope that it will be useful, but       #
+#          WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       #
+#                   General Public License for more details.                   #
+#                                                                              #
+#      You should have received a copy of the GNU General Public License       #
+#      along with GMTScripts. If not, see <http://www.gnu.org/licenses/>.      #
+################################################################################
 import pygmsh as pg
 import numpy as np
 from collections import OrderedDict
@@ -5,7 +22,7 @@ from collections import OrderedDict
 """
 Converts a profile obtained in ParaView to a GMSH *geo file. Requires pygmsh.
 
-IMPORTANT: Currently only works for x=const, i.e. parallely to y-axis. See 
+IMPORTANT: Currently only works for x=const, i.e. parallely to y-axis. See
 TODO.
 """
 
@@ -117,4 +134,3 @@ def process(layers, BCs, lcar, geo, vscale=1):
 
 if __name__ ==  '__main__':
     pass
-
