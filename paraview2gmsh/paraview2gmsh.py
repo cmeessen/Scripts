99b592ce05c1816ba309d9f3c7b3187cd7217570
Christian Meeßen <14222414+cmeessen@users.noreply.github.com>
2019-03-16 11:21:57 +0100

    PEP8 conformity of comments

--- paraview2gmsh/paraview2gmsh.py
+++ paraview2gmsh/paraview2gmsh.py
@@ -1,20 +1,20 @@
-################################################################################
-#                     Copyright (C) 2019 by Christian Meeßen                   #
-#                                                                              #
-#                         This file is part of Scripts                         #
-#                                                                              #
-#       GMTScripts is free software: you can redistribute it and/or modify     #
-#     it under the terms of the GNU General Public License as published by     #
-#           the Free Software Foundation version 3 of the License.             #
-#                                                                              #
-#      GMTScripts is distributed in the hope that it will be useful, but       #
-#          WITHOUT ANY WARRANTY; without even the implied warranty of          #
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       #
-#                   General Public License for more details.                   #
-#                                                                              #
-#      You should have received a copy of the GNU General Public License       #
-#       along with Scripts. If not, see <http://www.gnu.org/licenses/>.        #
-################################################################################
+###############################################################################
+#                     Copyright (C) 2019 by Christian Meeßen                  #
+#                                                                             #
+#                         This file is part of Scripts                        #
+#                                                                             #
+#       GMTScripts is free software: you can redistribute it and/or modify    #
+#     it under the terms of the GNU General Public License as published by    #
+#           the Free Software Foundation version 3 of the License.            #
+#                                                                             #
+#      GMTScripts is distributed in the hope that it will be useful, but      #
+#          WITHOUT ANY WARRANTY; without even the implied warranty of         #
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      #
+#                   General Public License for more details.                  #
+#                                                                             #
+#      You should have received a copy of the GNU General Public License      #
+#       along with Scripts. If not, see <http://www.gnu.org/licenses/>.       #
+###############################################################################
 import pygmsh as pg
 import numpy as np
 from collections import OrderedDict
