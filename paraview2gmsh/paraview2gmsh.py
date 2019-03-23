4021361fd6df93af7fcf117e14af4f33faaf78f9
Christian Meeßen <14222414+cmeessen@users.noreply.github.com>
2019-03-23 08:57:02 +0100

    Added left and right boundaries

--- paraview2gmsh/paraview2gmsh.py
+++ paraview2gmsh/paraview2gmsh.py
@@ -20,7 +20,8 @@ import numpy as np
 from collections import OrderedDict
 
 """
-Converts a profile obtained in ParaView to a GMSH *geo file. Requires pygmsh.
+Converts a profile obtained from a GMS model in ParaView to a GMSH *geo file.
+Requires pygmsh. 
 
 IMPORTANT: Currently only works for x=const, i.e. parallely to y-axis. See
 TODO.
@@ -51,50 +52,66 @@ def add_surface(filename, name, geom, lcar, BC=None, vscale=1):
 
     Returns
     -------
-
+    line_BC_left, line_BC_right : lists
+        Lists of lines that constitute the left and right boundary
     """
     print('Processing', filename)
-    vertical_offset = 0
-    if BC == 'lower':
-        # TODO: Is this necessary? In the example this avoids meshing problems
-        #       at the lower boundary. Maybe GMSHs `Coherence` doesnt
-        #       properly work?
-        vertical_offset = 1
     # Important: columns are loaded in order y,z,x!
-    body = np.loadtxt(filename, delimiter=',', skiprows=1, usecols=[1, 2, 0])
+    body = np.genfromtxt(filename, delimiter=',', skip_header=1,
+                         usecols=[1,2,0], names=['y','z','x'])
+    # Sort so that y-coords are increasing
+    body.sort(order=['y','z'])
+    # Convert back to a ndarray
+    oldshape = body.shape
+    body = body.view(np.float64).reshape(oldshape+(-1,))
     body[:, 1] *= vscale
     body[:, 2] = 0
     coords_top = body[1::2]
-    coords_bot = body[::2] - vertical_offset
+    coords_bot = body[::2]
     coords = np.concatenate((coords_top, coords_bot[::-1]))
     # Add points
     points = []
     lines = []
+
     print('> adding points')
     for row in coords:
         points.append(geom.add_point(row, lcar))
+
     print('> creating lines')
     for i in range(len(points)-1):
         lines.append(geom.add_line(points[i], points[i+1]))
     lines.append(geom.add_line(points[-1], points[0]))
+
+    line_BC_left = None
+    line_BC_right = None
+    p0, p1 = lines[-1].points
+    if np.linalg.norm(p0.x - p1.x) > 0:
+        line_BC_left = lines[-1]
+    line_id = int(len(lines)/2 - 1)
+    p0, p1 = lines[line_id].points
+    if np.linalg.norm(p0.x - p1.x) > 0:
+        line_BC_right = lines[line_id]
+
     if BC == 'upper':
         print('> defining upper BC `Top`')
         geom.add_physical_line(lines[:coords_top.shape[0]-1], label='Top')
     elif BC == 'lower':
         print('> defining lower BC `Bottom`')
         geom.add_physical_line(lines[coords_top.shape[0]:-1], label='Bottom')
-    # Add line loop
+
     print('> creating line loops')
     ll = geom.add_line_loop(lines)
-    # Make surface
+
     print('> creating surface')
     ps = geom.add_plane_surface(ll)
-    # Make physical surface
+
     print('> assigning physcial surface name', name)
     geom.add_physical_surface(ps, label=name)
 
+    return line_BC_left, line_BC_right
 
-def process(layers, BCs, lcar, geo, vscale=1):
+
+def process(layers, BCs, lcar, geo, vscale=1, tolerance=1e-7):
     """
     Start the conversion process.
 
@@ -112,19 +129,30 @@ def process(layers, BCs, lcar, geo, vscale=1):
         Output file name
     vscale : float
         Scale the data z-axis by this value.
+    tolerance : float
+        The tolerance for gmsh to remove invalid geometries. This might have to
+        be adjusted depending on the mesh.
     """
     geom = pg.built_in.Geometry()
+    geom.add_raw_code('Geometry.AutoCoherence = 2;')
+    geom.add_raw_code('Geometry.Tolerance = '+str(tolerance)+';')
 
+    line_BC_left = []
+    line_BC_right = []
     # Obtain the layer polygons
     for layer in layers.keys():
         if layer in BCs.keys():
             BC = BCs[layer]
         else:
             BC = None
-        add_surface(layers[layer], layer, geom, lcar, BC, vscale)
-
-    # geom.add_raw_code("Geometry.Tolerance = 1;")
-    geom.add_raw_code("Coherence;")
+        left, right = add_surface(layers[layer], layer, geom, lcar, BC, vscale)
+        if left is not None:
+            line_BC_left.append(left)
+        if right is not None:
+            line_BC_right.append(right)
+    geom.add_physical_line(line_BC_left, label='Left')
+    geom.add_physical_line(line_BC_right, label='Right')
+    geom.add_raw_code('Coherence;')
 
     # Write the file
     f = open(geo, mode='w')
